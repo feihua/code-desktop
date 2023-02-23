44ac191fb00c6df75afe516ffe33318ff066d7ce
koobe <koobe.liu@uaf.com.cn>
2023-02-23 13:46:34 +0800

    完善ui

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,9 @@
       <el-form-item label="前端路径">
         <el-input v-model="gerateForm.font_path_name" placeholder="D:\workspace\demo\src\main\java\com\example"/>
       </el-form-item>
+      <el-form-item label="作者">
+        <el-input v-model="gerateForm.author" placeholder="koobe"/>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">生成curd</el-button>
       </el-form-item>
@@ -32,20 +35,21 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-import { invoke } from "@tauri-apps/api/tauri";
-import { ElMessage } from 'element-plus'
+import {reactive} from 'vue'
+import {invoke} from "@tauri-apps/api/tauri";
+import {ElMessage} from 'element-plus'
 import 'element-plus/es/components/message/style/css'
 // import Greet from './components/Greet.vue';
 
 const gerateForm = reactive({
   db_url: 'mysql://root:r-wz9wop62956dh5k9ed@rm-wz9a2yv489d123yqkdo.mysql.rds.aliyuncs.com:3306',
   db_name: 'rustdb',
-  db_table: 'sys_user,sys_role',
+  db_table: 'sys_user,sys_role_user,sys_role,sys_menu_role,sys_menu',
   package_name: 'com.example.springboottpl',
   path_name: 'D:\\workspace\\java\\spring-boot-tpl\\src\\main\\java\\com\\example\\springboottpl\\',
   t_prefix: 'sys_',
   font_path_name: 'E:\\generate\\',
+  author: '刘飞华',
 })
 const onSubmit = () => {
   console.log('submit!', gerateForm.db_name)
@@ -56,15 +60,15 @@ const onSubmit = () => {
     packageName: gerateForm.package_name,
     savePath: gerateForm.path_name,
     tPrefix: gerateForm.t_prefix,
-    fontPathName: gerateForm.font_path_name
-  })
-    .then(() =>
+    fontPathName: gerateForm.font_path_name,
+    author: gerateForm.author
+  }).then(() =>
       ElMessage({
         message: '生成curd代码成功',
         type: 'success',
         offset: 200,
       })
-    );
+  );
 
 }
 </script>
